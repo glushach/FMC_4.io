6589585150e22852f74ecee34d2df55c63330ff5
Aleksandr <shoppingplaneta@gmail.com>
2020-07-06 19:11:24 +0300

    Сделал Хедер и ПромоХедер

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
   $('.carousel__inner').slick({
-    dots: false, //точеки внизу слайдера
+    dots: true, //точеки внизу слайдера
     speed: 1200,
     adaptiveHeight: false, //высота слайда будет одинаковая, в независимости от высоты контента
-    autoplay: true,
-    autoplaySpeed: 2000,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
     fade: true,
     cssEase: 'linear',
-    prevArrow: '<button type="button" class="slick-prev"><img src="../img/reviews/prev-icon.png" ,alt=""></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="../img/reviews/next-icon.png" ,alt=""></button>',
+    prevArrow: '<button type="button" class="slick-prev"><img src="../icons/promoHeader-icon/prev-icon.png" ,alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="../icons/promoHeader-icon/next-icon.png" ,alt=""></button>',
     responsive: [
       {
         breakpoint: 768,
