d0899021a0e77719b79802ffcdfa1b0240c4426f
Aleksandr <shoppingplaneta@gmail.com>
2020-07-10 21:14:20 +0300

    Делаю слайдер

--- src/js/script.js
+++ src/js/script.js
@@ -20,8 +20,33 @@ $(document).ready(function(){
     ]
   });
 
-  $('input[name=phone]').mask("+7 (999) 999-99-99");
+  $('.right').click(function(){
+    let currentImage = $('.mySlider-item.curry');
+    let currentImageIndex = $('.mySlider-item.curry').index();
+    let rightImageIndex = currentImageIndex + 1;
+    let rightImade = $('.mySlider-item').eq(rightImageIndex);
+    currentImage.fadeOut(1000);
+    currentImage.removeClass('curry');
 
+    if(rightImageIndex == ($('.mySlider-item:last').index()+1)){
+      $('mySlider-item').eq(0).fadeIn(1000);
+      $('mySlider-item').eq(0).addClass('curry');
+    } else {
+      rightImade.fadeIn(1000);
+      rightImade.addClass('curry');
+    }
+  });
 
+  $('.left').click(function() {
+		let currentImage = $('.mySlider-item.curry');
+		let currentImageIndex = $('.mySlider-item.curry').index();
+		let leftImageIndex = currentImageIndex - 1;
+		let leftImage = $('.mySlider-item').eq(leftImageIndex);
+  
+    currentImage.fadeOut(1000);
+		currentImage.removeClass('curry');
+		leftImage.fadeIn(1000);
+		leftImage.addClass('curry');
+  });
+  
 });
-
