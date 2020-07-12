9d2d03ee114231f619e7ad781eac30bdf263549a
Aleksandr <shoppingplaneta@gmail.com>
2020-07-12 17:37:04 +0300

    Создал секцию Эксперт Солюшинс и алдаптирова ее под мобилку

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+"use strict";
 $(document).ready(function(){
   $('.carousel__inner').slick({
     dots: true, //точеки внизу слайдера
@@ -19,34 +20,4 @@ $(document).ready(function(){
       }
     ]
   });
-
-  $('.right').click(function(){
-    let currentImage = $('.mySlider-item.curry');
-    let currentImageIndex = $('.mySlider-item.curry').index();
-    let rightImageIndex = currentImageIndex + 1;
-    let rightImade = $('.mySlider-item').eq(rightImageIndex);
-    currentImage.fadeOut(1000);
-    currentImage.removeClass('curry');
-
-    if(rightImageIndex == ($('.mySlider-item:last').index()+1)){
-      $('mySlider-item').eq(0).fadeIn(1000);
-      $('mySlider-item').eq(0).addClass('curry');
-    } else {
-      rightImade.fadeIn(1000);
-      rightImade.addClass('curry');
-    }
-  });
-
-  $('.left').click(function() {
-		let currentImage = $('.mySlider-item.curry');
-		let currentImageIndex = $('.mySlider-item.curry').index();
-		let leftImageIndex = currentImageIndex - 1;
-		let leftImage = $('.mySlider-item').eq(leftImageIndex);
-  
-    currentImage.fadeOut(1000);
-		currentImage.removeClass('curry');
-		leftImage.fadeIn(1000);
-		leftImage.addClass('curry');
-  });
-  
 });
