23cc3c52c966f013ae9a3c2e1f1b4e84b6c90ccc
Aleksandr <shoppingplaneta@gmail.com>
2020-10-07 16:34:31 +0300

    slick refactoring code

--- dist/js/script.js
+++ dist/js/script.js
@@ -8,8 +8,8 @@ $(document).ready(function(){
     // autoplaySpeed: 2000,
     fade: true,
     cssEase: 'linear',
-    prevArrow: '<button type="button" class="slick-prev"><img src="../icons/promoHeader-icon/prev-icon.png" ,alt=""></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="../icons/promoHeader-icon/next-icon.png" ,alt=""></button>',
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/promoHeader-icon/prev-icon.png" ,alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/promoHeader-icon/next-icon.png" ,alt=""></button>',
     responsive: [
       {
         breakpoint: 768,
